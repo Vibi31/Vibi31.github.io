8aa658b8e755a6c36cffe078739e5e613550f65d
Vibishan <vrvichu0@gmail.com>
2025-08-24 16:32:52 +0200

    push new thumbnails
    update

--- images/create_thumb.py
+++ images/create_thumb.py
@@ -1,25 +1,35 @@
-from PIL import Image
+from PIL import Image, ExifTags
 import os
 
-# Paths
-input_folder = "images"      # where your original images are
-output_folder = "images/thumbs"  # where thumbnails will be saved
-thumb_size = (600, 600)      # thumbnail size (change as needed)
+input_folder = "images"
+output_folder = "images/thumbs"
+thumb_size = (600, 600)
 
-# Make sure output folder exists
 os.makedirs(output_folder, exist_ok=True)
 
-# Loop through all files in input folder
 for filename in os.listdir(input_folder):
-    if filename.lower().endswith((".jpg", ".jpeg", ".png", ".webp")):
+    if filename.lower().endswith((".jpg", ".jpeg", ".png")):
         img_path = os.path.join(input_folder, filename)
         img = Image.open(img_path)
 
-        # Create thumbnail (keeps aspect ratio)
-        img.thumbnail(thumb_size)
+        # Fix EXIF orientation
+        try:
+            for orientation in ExifTags.TAGS.keys():
+                if ExifTags.TAGS[orientation]=='Orientation':
+                    break
+            exif=dict(img._getexif().items())
 
-        # Save to thumbs folder
+            if exif[orientation] == 3:
+                img = img.rotate(180, expand=True)
+            elif exif[orientation] == 6:
+                img = img.rotate(270, expand=True)
+            elif exif[orientation] == 8:
+                img = img.rotate(90, expand=True)
+        except:
+            pass
+
+        # Create thumbnail
+        img.thumbnail(thumb_size)
         out_path = os.path.join(output_folder, filename)
         img.save(out_path, "JPEG", quality=85)
-
         print(f"Created thumbnail: {out_path}")
